87ce325020d91bc2d075b2e6971a6d4c4e28f99c
thathardwareguy <ardutech11@gmail.com>
2022-08-30 01:05:05 +0100

    First deploy to repo

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'bwk260ht51';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-j-a3vs1u.us.auth0.com',            // Auth0 domain
+  clientId: 'MZ7ycwwKnzbM16Zu18X4TDFG9HnQtOnl',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+};
